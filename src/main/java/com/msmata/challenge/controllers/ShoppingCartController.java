67918ffcc50c9ad944883b54f56fd7e18aa2ec67
Martin Mata <mmata@technisys.com>
2025-07-07 14:21:21 -0300

    Agrega anotaciones de documentacion de Swagger a ShoppingCartController

--- src/main/java/com/msmata/challenge/controllers/ShoppingCartController.java
+++ src/main/java/com/msmata/challenge/controllers/ShoppingCartController.java
@@ -3,6 +3,7 @@ package com.msmata.challenge.controllers;
 import com.msmata.challenge.entities.ShoppingCart;
 import com.msmata.challenge.requests.CreateCartRequest;
 import com.msmata.challenge.services.ShoppingCartService;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/carts")
+@Api(tags = "Carrito de compras")
 public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
@@ -21,36 +23,56 @@ public class ShoppingCartController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ShoppingCart> getCart(@PathVariable String id) {
+    @ApiOperation("Obtener un carrito de compras por id de carrito")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Carrito encontrado"),
+            @ApiResponse(code = 404, message = "Carrito no encontrado")
+    })
+    public ResponseEntity<ShoppingCart> getCart(@ApiParam("ID del carrito") @PathVariable String id) {
         return ResponseEntity.ok(shoppingCartService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<ShoppingCart> createCart(@RequestBody CreateCartRequest request) {
+    @ApiOperation("Crear un carrito de compras para un userID determinado")
+    public ResponseEntity<ShoppingCart> createCart(@ApiParam("Request con ID del usuario") @RequestBody CreateCartRequest request) {
         ShoppingCart saved = shoppingCartService.createCart(request.getUserId());
         return ResponseEntity.ok(saved);
     }
 
     @PutMapping("/{cartId}/product/{productId}")
-    public ResponseEntity<ShoppingCart> updateCartProducts(@PathVariable String cartId, @PathVariable String productId) {
+    @ApiOperation("Agregar un producto a un carrito de compras")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Carrito actualizado"),
+            @ApiResponse(code = 404, message = "Carrito no encontrado"),
+            @ApiResponse(code = 404, message = "Producto no encontrado")
+    })
+    public ResponseEntity<ShoppingCart> updateCartProducts(@ApiParam("ID del carrito") @PathVariable String cartId,@ApiParam("ID del producto")  @PathVariable String productId) {
         ShoppingCart shoppingCart = shoppingCartService.addProductToCart(cartId, productId);
         return ResponseEntity.ok(shoppingCart);
     }
 
     @DeleteMapping("/{cartId}/product/{productId}")
-    public ResponseEntity<ShoppingCart> removeProduct(@PathVariable String cartId, @PathVariable String productId) {
+    @ApiOperation("Remover un producto de un carrito de compras")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Carrito actualizado"),
+            @ApiResponse(code = 404, message = "Carrito no encontrado"),
+            @ApiResponse(code = 404, message = "Producto no encontrado")
+    })
+    public ResponseEntity<ShoppingCart> removeProduct(@ApiParam("ID del carrito") @PathVariable String cartId, @ApiParam("ID del producto") @PathVariable String productId) {
         ShoppingCart shoppingCart = shoppingCartService.removeProduct(cartId, productId);
         return ResponseEntity.ok(shoppingCart);
     }
 
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<ShoppingCart>> listShoppingCarts(@PathVariable String userId) {
+    @ApiOperation("Listar los carritos de compras de un userID determinado")
+    public ResponseEntity<List<ShoppingCart>> listShoppingCarts(@ApiParam("ID del usuario") @PathVariable String userId) {
         List<ShoppingCart> userCarts = shoppingCartService.getUserCarts(userId);
         return ResponseEntity.ok(userCarts);
     }
 
     @PostMapping("/{id}/process")
-    public ResponseEntity<Map<String, String>> processCart(@PathVariable String id) {
+    @ApiOperation("Procesar los articulos de un determinado carrito de compras")
+    public ResponseEntity<Map<String, String>> processCart(@ApiParam("ID del carrito") @PathVariable String id) {
         shoppingCartService.processOrder(id);
         Map<String, String> response = new HashMap<>();
         response.put("message", "Estamos procesando su orden");
